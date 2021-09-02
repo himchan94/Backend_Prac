a5cb962a4c2b6b6dcb2be6e785c21691d8c44f41
healthcarefrontend <himchan9494@gmail.com>
2021-09-02 23:54:07 +0900

    DB

--- Mall/index.js
+++ Mall/index.js
@@ -35,7 +35,6 @@ app.get("/mongodb", async (req, res) => {
 
 // longer supported options.Mongoose 6 always behaves as if useNewUrlParser,
 // useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false.
-// Please remove these options from your code.
 
 app.get("/test", (req, res) => {
   let name = req.query.name;
